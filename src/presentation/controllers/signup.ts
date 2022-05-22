6a885aab5e3090a638abf8b6a71d73f68f274f14
Ramzi <ramzi.hannachi1991@gmail.com>
2022-05-22 11:25:26 +0200

    refacto(signup) improve controller implementation

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,10 @@
 import { HttpRequest, HttpResponse } from "../protocols/http";
 import { MissingParamsError } from "../errors/missingParamsError";
 import { badRequest } from "../helpers/httpHelper";
+import { Controller } from "../protocols/controller";
 
-export class SignUpController {
-  handle(httprequest: HttpRequest): HttpResponse | undefined {
+export class SignUpController implements Controller {
+  handle(httprequest: HttpRequest): HttpResponse {
     const requiredFields = [
       "name",
       "email",
@@ -16,5 +17,11 @@ export class SignUpController {
         return badRequest(new MissingParamsError(field));
       }
     }
+
+    // TODO remove in future devs
+    return {
+      statusCode: 500,
+      body: "TODO",
+    };
   }
 }
