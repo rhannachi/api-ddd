a7575b9e1d82e8c4d16acdffcd094f6e402869f6
Ramzi <ramzi.hannachi1991@gmail.com>
2022-05-25 17:40:47 +0200

    feat(emailValidator): ensure EmailValidatorAdapter returns true if validator returns true

--- src/utilis/emailValidator.adapter.spec.ts
+++ src/utilis/emailValidator.adapter.spec.ts
@@ -1,9 +1,28 @@
-import { EmailValidatorAdapter } from "./emailValidator";
+import validator from "validator";
+import { EmailValidatorAdapter } from "./emailValidator.adapter";
+
+const makeEmailValidatorAdapter = () => {
+  return new EmailValidatorAdapter();
+};
 
 describe("EmailValidator", () => {
   test("Should return false if validator returns false", () => {
-    const sut = new EmailValidatorAdapter();
-    const isValid = sut.isValid("invalid_email@gmail.com");
+    const emailValidator = makeEmailValidatorAdapter();
+    jest.spyOn(validator, "isEmail").mockReturnValueOnce(false);
+    const isValid = emailValidator.isValid("invalid_email@gmail.com");
     expect(isValid).toBe(false);
+  });
+
+  test("Should return true if validator returns true", () => {
+    const emailValidator = makeEmailValidatorAdapter();
+    const isValid = emailValidator.isValid("valid_email@gmail.com");
+    expect(isValid).toBe(true);
+  });
+
+  test("Should call validator with correct email", () => {
+    const emailValidator = makeEmailValidatorAdapter();
+    const isEmailSpy = jest.spyOn(validator, "isEmail");
+    emailValidator.isValid("email@gmail.com");
+    expect(isEmailSpy).toHaveBeenCalledWith("email@gmail.com");
   });
 });
