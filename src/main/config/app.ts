3945919645ea71de7fd75b1be6dc60a9996406de
Ramzi <ramzi.hannachi1991@gmail.com>
2022-05-28 00:19:13 +0200

    feat: ensure return json and xml content-type

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import { bodyParser } from '../middleware/bodyParser'
+import { contentType } from '../middleware/contentType'
 import { cors } from '../middleware/cors'
 
 const app = express()
 app.use(bodyParser)
 app.use(cors)
+app.use(contentType)
 
 const port = process.env.PORT ?? 5050
 
